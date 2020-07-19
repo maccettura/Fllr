44e9c2670c1669148fd669c1f366aebed4583baa
Michelangelo Accettura III <>
2020-07-19 03:55:32 -0400

    Removing collapse from Navbar

--- www/src/components/NavBar.vue
+++ www/src/components/NavBar.vue
@@ -1,18 +1,12 @@
 <template>
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: #2B404E;">
+    <nav class="navbar fixed-top navbar-dark" style="background-color: #2B404E;">
       <div class="container-fluid">
           <a class="navbar-brand" href="#">Fllr.art</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav justify-content-end">
                 <li class="nav-item">
                     <a class="nav-link" target="_blank" href="https://github.com/maccettura/Fllr"> <img src="github.png" srcset="github-high.png 2x"> </a>
                 </li>
             </ul>
-        </div>
       </div>
     </nav>
 </template>
