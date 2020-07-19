5a806ff7e56d00690b785b8b3525937dc6f8b68c
Michelangelo Accettura III <>
2020-07-19 03:58:54 -0400

    Refactor to remove not needed components

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,34 +1,45 @@
 <template>
   <div id="app">
-    <NavBar />
-    <Masthead heading="Fllr.art" subheading="An image generation API for all your FPO image needs" />
-    <section id="documentation">
-      <div>
-
+    <nav class="navbar fixed-top navbar-dark" style="background-color: #2B404E;">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">Fllr.art</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" target="_blank" href="https://github.com/maccettura/Fllr"> <img src="github.png" srcset="github-high.png 2x"> </a>
+                </li>
+            </ul>
       </div>
-    </section>
-    <section id="try-it">
-      <LinkGenerator />
-    </section>
+    </nav>
+    <header class="masthead">
+      <div class="container h-100">
+          <div class="row h-100 align-items-center">
+              <div class="col-12 text-center">
+                  <h1 class="display-1">Fllr.art</h1>
+                  <p class="lead">An image generation API for all your FPO image needs</p>
+              </div>
+          </div>
+      </div>
+    </header>
+    <LinkGenerator />
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
-import Masthead from './components/Masthead.vue'
 import LinkGenerator from './components/LinkGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    Masthead,
     LinkGenerator
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #FCFCFC;
+}
+
 h1,
 h2,
 h3 {
@@ -42,7 +53,8 @@ h6 {
   color: #397AA3;
 }
 
-body {
-  background-color: #FCFCFC;
+.masthead {
+    height: 20vh;
+    min-height: 300px;
 }
 </style>
