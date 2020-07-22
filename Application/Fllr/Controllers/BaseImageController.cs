c38b500371d2208656c978a28dc57b4dd4d37e90
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2020-07-21 23:19:50 -0400

    Merge v1 into master (#2)
    * Updates for live deploy
    
    * Updating DockerFile
    
    * Adding new docker support
    
    * Fixing error handling
    
    * Modifying error handling, simplify routes
    
    * Update www site
    
    * Adjusting styling
    
    * Adding Notice file
    
    * Removing collapse from Navbar
    
    * Refactor to remove not needed components
    
    Co-authored-by: Michelangelo Accettura III <>

--- Application/Fllr/Controllers/BaseImageController.cs
+++ Application/Fllr/Controllers/BaseImageController.cs
@@ -1,10 +1,11 @@
 ﻿using Fllr.Application;
+using Fllr.Image.Generator;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fllr.Controllers
 {
     public class BaseImageController : ControllerBase
-    {
+    {        
         protected FileResult BuildResponse(PlaceholdImage image)
         {
             return File(image.Payload, image.MimeType);
---
+++ www/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <nav class="navbar fixed-top navbar-dark" style="background-color: #2B404E;">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">Fllr.art</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" target="_blank" href="https://github.com/maccettura/Fllr"> <img src="github.png" srcset="github-high.png 2x"> </a>
+                </li>
+            </ul>
+      </div>
+    </nav>
+    <header class="masthead">
+      <div class="container h-100">
+          <div class="row h-100 align-items-center">
+              <div class="col-12 text-center">
+                  <h1 class="display-1">Fllr.art</h1>
+                  <p class="lead">An image generation API for all your FPO image needs</p>
+              </div>
+          </div>
+      </div>
+    </header>
+    <LinkGenerator />
+  </div>
+</template>
+
+<script>
+import LinkGenerator from './components/LinkGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LinkGenerator
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #FCFCFC;
+}
+
+h1,
+h2,
+h3 {
+    font-family: 'Montserrat';
+    color: #262321;
+}
+
+h4,
+h5,
+h6 {
+  color: #397AA3;
+}
+
+.masthead {
+    height: 20vh;
+    min-height: 300px;
+}
+</style>
